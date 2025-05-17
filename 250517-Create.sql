40c8538fae5ab1b67b9e63ef44cb4dc9753b0284
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-05-17 13:23:19 -0300

    // att create database + schema

--- 250517-Create.sql
+++ 250517-Create.sql
@@ -1,3 +1,13 @@
+-- CREATE DATABASE
+
+CREATE DATABASE BANCO;
+
+-- CREATE SCHEMA (organiza e armazena todas as tabelas relacionadas ao esquema, como um categorizador)
+
+CREATE SCHEMA ESQUEMA;
+
+-- CREATE TABLE
+
 CREATE TABLE TABELA (
   INTEIRO INT PRIMARY KEY,
   STRING VARCHAR(250)
