1807a98a8d25e15cc55923283b5d70b8695b9aff
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-05-17 23:04:03 -0300

    // include column types

--- 250516-Select.sql
+++ 250516-Select.sql
@@ -5,6 +5,9 @@ SELECT * FROM tabela;
 -- SELECT COM CONDIÇÃO (WHERE)
 
 SELECT * FROM tabela WHERE coluna = 'X';
+SELECT * FROM tabela WHERE coluna_num <> 1;
+SELECT * FROM tabela WHERE coluna_string > 'C'; -- filtrará as colunas que começam com a letra C em diante
+SELECT * FROM tabela WHERE coluna_data > '2025-05-17';
 
 -- SELECT DESPREZANDO DADOS DUPLICADOS (DISTINCT)
 
