6766f1345c32e24127be0c4f18470eb4a4f5eb04
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-05-17 21:41:30 -0300

    // att multiple data inserts

--- 250517-Insert.sql
+++ 250517-Insert.sql
@@ -1,5 +1,20 @@
+-- INSERT ÚNICO
+
 INSERT INTO tabela (
   id,
   nome,
   telefone)
 VALUES ('1', 'Carol', '999999999');
+
+-- INSERT DE MÚLTIPLOS DADOS
+
+INSERT INTO tabela (
+  id,
+  nome,
+  telefone)
+VALUES
+  ('2', 'Guilherme', '999999998'),
+  ('3', 'Angela', '999999997'),
+  ('4', 'Jade', '999999996'),
+  ('5', 'Kiara', '999999995'),
+  ('6', 'Zeus', '999999994');
