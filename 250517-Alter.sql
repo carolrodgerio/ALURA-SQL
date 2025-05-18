ce89f04c029fd27717e2c1228c3aac5820afefdb
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-05-17 21:24:31 -0300

    // att foreign key

--- 250517-Alter.sql
+++ 250517-Alter.sql
@@ -5,3 +5,9 @@ ALTER TABLE tabela ADD coluna VARCHAR(250);
 -- exclusão de coluna
 
 ALTER TABLE tabela DROP COLUMN coluna;
+
+-- inclusão de FK
+
+ALTER TABLE tabela
+ADD COLUMN foreign_key INTEGER
+REFERENCES tabela2(id_tabela2);
