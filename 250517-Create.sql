67061d40299d0982b690404d12c28aeea47a2335
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-05-18 00:47:25 -0300

    // add on delete cascade

--- 250517-Create.sql
+++ 250517-Create.sql
@@ -16,3 +16,14 @@ CREATE TABLE tabela (
   data DATE,
   FOREIGN KEY (data) REFERENCES tabela2 (primary_key_tabela2)
 );
+
+-- CREATE TABLE COM DELETE CASCADE (quando a alguma linha da tabela pai for excluída, as linhas que referenciam ela nas tabelas-filho também serão)
+
+CREATE TABLE tabela (
+  inteiro INT PRIMARY KEY,
+  string VARCHAR(250),
+  texto TEXT,
+  num_decimal DECIMAL(10,2),
+  data DATE,
+  FOREIGN KEY (data) REFERENCES tabela2 (primary_key_tabela2) ON DELETE CASCADE
+);
