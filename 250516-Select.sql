af27f39a4a870a979626176a8d4b140b3d2da02e
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-05-17 23:07:53 -0300

    // include != operator

--- 250516-Select.sql
+++ 250516-Select.sql
@@ -5,7 +5,7 @@ SELECT * FROM tabela;
 -- SELECT COM CONDIÇÃO (WHERE)
 
 SELECT * FROM tabela WHERE coluna = 'X';
-SELECT * FROM tabela WHERE coluna_num <> 1;
+SELECT * FROM tabela WHERE coluna_num <> 1; -- <> significa diferente, também pode ser utilizado !=
 SELECT * FROM tabela WHERE coluna_string > 'C'; -- filtrará as colunas que começam com a letra C em diante
 SELECT * FROM tabela WHERE coluna_data > '2025-05-17';
 
