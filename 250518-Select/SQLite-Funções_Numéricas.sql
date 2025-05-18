f82eb9d0047494e9be18bf6fcfa50adea191f743
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-05-18 19:17:33 -0300

    // create SQLite-Funções_Numéricas.sql

---
+++ 250518-Select/SQLite-Funções_Numéricas.sql
@@ -0,0 +1,15 @@
+SELECT AVG(coluna), ROUND(AVG(coluna), 2) FROM tabela; -- arredonda o resultado em 2 casas decimais
+
+SELECT CEIL(coluna) FROM tabela; -- arredonda o resultado para o maior inteiro
+
+SELECT FLOOR(coluna) FROM tabela; -- arredonda o resultado para o menor
+
+SELECT POWER(2, 3); -- retorna o resultado de 2 elevado a 3
+
+SELECT SQRT(16); -- retorna a raiz quadrade de 16
+
+SELECT RANDOM(); -- gera um número inteiro aleatório
+
+SELECT ABS(-5); -- retorna o valor absoluto de -5 (5)
+
+SELECT HEX('string'); -- converte um número ou uma string para sua forma hexadecimal
