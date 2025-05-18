fa59be49675adb033e6e38f22efc687072da8fbe
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-05-18 18:58:19 -0300

    // att numeric functions

--- 250516-Select.sql
+++ 250516-Select.sql
@@ -127,6 +127,16 @@ SELECT CEIL(coluna) FROM tabela; -- arredonda o resultado para o maior inteiro
 
 SELECT FLOOR(coluna) FROM tabela; -- arredonda o resultado para o menor
 
+SELECT POWER(2, 3); -- retorna o resultado de 2 elevado a 3
+
+SELECT SQRT(16); -- retorna a raiz quadrade de 16
+
+SELECT RANDOM(); -- gera um número inteiro aleatório
+
+SELECT ABS(-5); -- retorna o valor absoluto de -5 (5)
+
+SELECT HEX('string'); -- converte um número ou uma string para sua forma hexadecimal
+
 
 -- // -- // -- // -- // -- // ORDEM DAS CLÁUSULAS // -- // -- // -- // -- // --
 
