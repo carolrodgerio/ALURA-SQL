4e22d90e28972d9638df341d345a3e9f96f56d09
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-05-17 21:20:02 -0300

    // att foreign key

--- 250517-Create.sql
+++ 250517-Create.sql
@@ -1,14 +1,18 @@
 -- CREATE DATABASE
 
-CREATE DATABASE BANCO;
+CREATE DATABASE banco;
 
 -- CREATE SCHEMA (organiza e armazena todas as tabelas relacionadas ao esquema, como um categorizador)
 
-CREATE SCHEMA ESQUEMA;
+CREATE SCHEMA esquema;
 
 -- CREATE TABLE
 
-CREATE TABLE TABELA (
-  INTEIRO INT PRIMARY KEY,
-  STRING VARCHAR(250)
+CREATE TABLE tabela (
+  inteiro INT PRIMARY KEY,
+  string VARCHAR(250),
+  texto TEXT,
+  num_decimal DECIMAL(10,2),
+  data DATE,
+  FOREIGN KEY (data) REFERENCES tabela2 (primary_key_tabela2)
 );
