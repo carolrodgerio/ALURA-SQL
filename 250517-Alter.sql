31a972550af207403c0646ebc449a921c4f38826
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-05-18 19:33:17 -0300

    // add rename function

--- 250517-Alter.sql
+++ 250517-Alter.sql
@@ -6,6 +6,12 @@ ALTER TABLE tabela ADD COLUMN coluna VARCHAR(250);
 
 ALTER TABLE tabela DROP COLUMN coluna;
 
+-- renomear tabela ou coluna
+
+ALTER TABLE tabela RENAME TO nova_tabela;
+
+ALTER TABLE tabela RENAME COLUMN coluna TO nova_coluna;
+
 -- inclusão de FK
 
 ALTER TABLE tabela
