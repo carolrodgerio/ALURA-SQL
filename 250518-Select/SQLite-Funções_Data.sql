2ac364506ba3ec33012ffcf6c2ef7f82925c2d03
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-05-19 20:28:36 -0300

    // add date formats

--- 250518-Select/SQLite-Funções_Data.sql
+++ 250518-Select/SQLite-Funções_Data.sql
@@ -1,5 +1,13 @@
 SELECT STRFTIME('%Y/%M', coluna_data) FROM tabela; -- altera a formatação da data
 
+/*
+%Y: ano com 4 dígitos
+%m: mês com 2 dígitos
+%d: dia com 2 dígitos
+%H: hora (00-24)
+%M: minuto
+*/
+
 SELECT JULIANDAY(coluna_fim) - JULIANDAY(coluna_inicio) FROM tabela; -- retorna a diferença de dias entre duas colunas de data
 
 SELECT DATE('now'); -- retorna a data atual
